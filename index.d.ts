3905a43d4c2bbcf438c9d5289fadad5b5166d26b
Zheng Chen <zheng.chen@yitu-inc.com>
2018-10-27 18:18:55 +0800

    operator $splice typing fixed

--- index.d.ts
+++ index.d.ts
@@ -51,7 +51,7 @@ export type Spec<T, C extends CustomCommands<object> = never> =
 type ArraySpec<T, C extends CustomCommands<object>> =
   | { $push: T[] }
   | { $unshift: T[] }
-  | { $splice: Array<[number] | [number, number] | [number, number, T]> }
+  | { $splice: Array<[number, number?] | [number, number, ...T[]]> }
   | { [index: string]: Spec<T, C> }; // Note that this does not type check properly if index: number.
 
 type MapSpec<K, V> =
